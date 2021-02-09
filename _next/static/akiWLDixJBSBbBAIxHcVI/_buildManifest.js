ecbd09453bc45775bc7f0483ef8a835e7ed2ae2d
Yi Ding <yi.s.ding@gmail.com>
2021-02-08 19:40:23 -0800

    Updated website

---
+++ _next/static/akiWLDixJBSBbBAIxHcVI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F3438d7cff0b99269d135.css","static\u002Fchunks\u002Fpages\u002Findex-15069c80426da6cdf5dc.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-efa460e7e8678ca0f72c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
