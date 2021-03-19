e4f32a925aa2e1ecae08fe917532d778808d7a6f
Yi Ding <yi.s.ding@gmail.com>
2021-03-19 07:55:08 -0700

    updated website

---
+++ _next/static/6yB0gy0-pYqIKgQCFyKMZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-020204519cc7f26ddb24.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-efa460e7e8678ca0f72c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cd2b0f5bf57786913860";
+/******/ 	var hotCurrentHash = "a489450419426dfc273d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
